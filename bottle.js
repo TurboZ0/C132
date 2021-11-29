71fe92a22cf390a20a42905c5b01de2799dd39f8
TurboZ0 <87539784+TurboZ0@users.noreply.github.com>
2021-11-29 16:14:55 +0530

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,31 @@
+z=[];
+function preload() {
+    load_image="Bottles.jpg"
+}
+
+function setup() {
+    canvas=createCanvas(600,500);
+    canvas.position(700,300);
+
+    loading_model= ml5.objectDetector("cocossd", modelHasLoaded);
+document.getElementById("status").innerHTML="Status: Patience Is key...";
+}
+
+function modelHasLoaded() {
+    console.log("Cocossd is a buiscuit's name");
+    status1=true;
+    loading_model.detect(hold_image, recieve_result);
+}
+function recieve_result(error,result) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+      console.log(result);
+        z=result;
+    }
+    }
+
+function draw() {
+    image(video, 0, 0, 600, 600);
+}
